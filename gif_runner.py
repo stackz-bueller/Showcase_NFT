06245665eae41094611c2b687f8f3d21827f0f58
stackz-bueller <34289879+stackz-bueller@users.noreply.github.com>
2019-12-12 18:35:18 -0500

    Create gif_runner.py
    script to run gif on command

---
+++ gif_runner.py
@@ -0,0 +1,81 @@
+from urllib.request import Request, urlopen
+import os
+
+def store_gif(art):
+    fname = 'tmp.gif'
+    resp = urlopen(Request(art.ImageURL,headers={'User-Agent': 'Mozilla/5.0'})).read()
+    open(fname,"wb+").write(resp)
+    
+def cleanup():
+    exists = os.path.isfile('tmp.gif')
+    if exists:
+        os.remove('tmp.gif')
+        print('Files cleaned up!')
+
+def get_gif():
+    gif = mimread(fname, memtest = False)
+    nums = len(gif)
+    print("Total {} frames in the gif!".format(nums))
+
+      max_height = 0
+      max_width = 0
+
+      for frame in gif:
+          h = frame.shape[0]
+          w = frame.shape[1]
+
+          if h > max_height:
+              max_height = h
+          if w > max_width:
+              max_width = w
+
+      if(max_height >= 900 or max_width >= 1500):
+          if(max_height >= 900):
+              scale = 800 / max_height
+              dim = (int(scale * max_height), max_width)
+          elif(max_width >= 1500):
+              scale = 1500 / max_width
+              dim = (max_height, int(scale * max_width))
+
+      else:
+          dim = (900,900)
+
+      imgs = [cv2.cvtColor(img, cv2.COLOR_RGB2BGR) for img in gif]
+      resized = [cv2.resize(img, dim,
+      interpolation = cv2.INTER_AREA) for img in imgs]
+      return [imgs, resized]
+      
+def show_first_image(art, gif):
+    try:
+        cv2.imshow('{} gif'.format(art.Name), gif[0])
+        if cv2.waitKey(100)&0xFF == 27:
+            cv2.waitKey(1)
+            cv2.destroyAllWindows()
+            cv2.waitKey(1)
+    except:
+        print('Problem showing Gif')
+        
+    finally:
+        return ''
+      
+def run_gif(art, gif):
+    i = 1
+    try:
+        while True:
+            cv2.imshow('{} gif'.format(art.Name), gif[i])
+            if cv2.waitKey(100)&0xFF == 27:
+                break
+            time.sleep(0.1)
+            i = (i+1)%nums
+
+        cv2.destroyAllWindows()
+        cv2.waitKey(1)
+        
+    except:
+        print('Problem running Gif!')
+
+    finally:
+        cv2.waitKey(1)
+        cv2.destroyAllWindows()
+        cv2.waitKey(1)
+        return ''
